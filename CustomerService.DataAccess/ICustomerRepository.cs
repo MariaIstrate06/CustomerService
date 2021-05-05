876547d204000876ef583aa449faf82d67835ede
Balan, Elena <elena.balan@centric.eu>
2021-05-05 22:15:29 +0300

    fix solution

--- CustomerService.DataAccess/ICustomerRepository.cs
+++ CustomerService.DataAccess/ICustomerRepository.cs
@@ -14,6 +14,5 @@ namespace CustomerService.DataAccess
         Task<Customer> InsertCustomerAsync(Customer customer);
         Task<bool> UpdateCustomerAsync(Customer customer);
         Task<bool> DeleteCustomerAsync(int id);
-        Task InsertCustomerAsync(Customer customer);
     }
 }
